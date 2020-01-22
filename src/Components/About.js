295481cc5499c9021112cc16758383c286ad6d7a
Joshua Grossman <Josh1794@gmail.com>
2020-01-22 15:55:07 -0500

    changed resume button

--- src/Components/About.js
+++ src/Components/About.js
@@ -50,7 +50,7 @@ class About extends Component {
               <div className="columns download">
                 <p>
                   <a href={resumeDownload} className="button">
-                    <i className="fa fa-download"></i>Download Resume
+                    <i className="fa fa-download"></i>My Resume
                   </a>
                 </p>
               </div>
